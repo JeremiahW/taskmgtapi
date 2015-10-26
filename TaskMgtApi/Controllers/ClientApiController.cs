709c08ed83a9d8fe5f6fb8dcb0b2e1fdfb761a01
wangjixiao@gmail.com <wangjixiao@gmail.com>
2015-10-26 16:03:25 +0800

    Add Client Api

--- TaskMgtApi/Controllers/ClientApiController.cs
+++ TaskMgtApi/Controllers/ClientApiController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Web;
 using System.Web.Http;
 using Newtonsoft.Json;
 using TaskMgt.DataAccess;
@@ -13,10 +14,12 @@ namespace TaskMgtApi.Controllers
     public class ClientApiController : ApiController
     {
         private pdxtEntities _ctx;
+        private HttpContext _context;
 
         public ClientApiController()
         {
             this._ctx = new pdxtEntities();
+            this._context = HttpContext.Current;
         }
 
         [AcceptVerbs(WebRequestMethods.Http.Get, WebRequestMethods.Http.Post)]
@@ -43,6 +46,37 @@ namespace TaskMgtApi.Controllers
             return response;
         }
 
-  
+        [HttpGet, HttpPost]
+        [Route("api/ClientApi/Add")]
+        public HttpResponseMessage Add()
+        {
+            string cname = _context.Request["cname"]?.ToString() ?? "";
+            string pinyin = _context.Request["pinyin"]?.ToString() ?? "";
+            string address = _context.Request["address"]?.ToString() ?? "";
+            string contact = _context.Request["contact"]?.ToString() ?? "";
+            string mobile = _context.Request["mobile"]?.ToString() ?? "";
+            string tel = _context.Request["tel"]?.ToString() ?? "";
+            string fax = _context.Request["fax"]?.ToString() ?? "";
+
+            Customer model = new Customer();
+            model.name = cname;
+            model.pinyin = pinyin;
+            model.addr = address;
+            model.contact = contact;
+            model.mobile = mobile;
+            model.tel = tel;
+            model.fax = fax;
+
+            _ctx.Customers.Add(model);
+            _ctx.SaveChanges();
+            var response = new HttpResponseMessage
+            {
+                Content = new StringContent(JsonConvert.SerializeObject(model.id)),
+            };
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/json");
+            return response;
+
+        }
+
     }
 }
